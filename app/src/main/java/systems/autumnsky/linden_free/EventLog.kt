08ee35792d9332046f115ab3df663e25884a315c
Akiya <autumnsky64@gmail.com>
2020-06-24 14:13:33 +0900

    起床・入眠のインサート・アップデートをRealmクラス側に移設

--- app/src/main/java/systems/autumnsky/linden_free/EventLog.kt
+++ app/src/main/java/systems/autumnsky/linden_free/EventLog.kt
@@ -1,7 +1,10 @@
 package systems.autumnsky.linden_free
 
+import io.realm.Realm
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
 import java.util.*
 
 open class EventLog (
@@ -9,4 +12,36 @@ open class EventLog (
     open var time: Date? = null,
     open var event_name: String? = null,
     open var quantity: Double? = null
-): RealmObject()
+): RealmObject() {
+
+    fun insert(event: String?, cal: Calendar){
+        val realm = Realm.getDefaultInstance()
+        val newId: Long = (realm.where<EventLog>().max("id")?.toLong()?:0) + 1
+
+        realm.executeTransaction {
+            val eventLog = realm.createObject<EventLog>(newId).apply{
+                time = cal.time
+                event_name = event
+            }
+            realm.copyToRealm(eventLog)
+        }
+        realm.close()
+    }
+
+    fun update(event: String?, oldDate: Date, newCal: Calendar){
+        val realm = Realm.getDefaultInstance()
+
+        realm.executeTransaction{
+            realm.where<EventLog>()
+                .greaterThanOrEqualTo("time", oldDate)
+                .equalTo("event_name", event)
+                .findFirst()?.apply {
+
+                    time = newCal.time
+
+                }
+        }
+
+        realm.close()
+    }
+}
