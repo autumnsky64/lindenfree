032538bad2e76ffc20208f0308f88937a548cb9f
Akiya <autumnsky64@gmail.com>
2020-06-27 10:51:18 +0900

    #60 FABの動作テストを追加

---
+++ app/src/androidTest/java/systems/autumnsky/linden_free/Log_Fab_test.kt
@@ -0,0 +1,115 @@
+package systems.autumnsky.linden_free
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class Log_Fab_test {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun log_Fab_test() {
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.navigation_log), withContentDescription("Log"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.insert_event),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.activity_log),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    9
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
+
+        val constraintLayout = onView(
+            allOf(
+                withId(R.id.action_row_in_bottom_sheet),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.action_list),
+                        childAtPosition(
+                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                            0
+                        )
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        constraintLayout.perform(click())
+
+        val appCompatButton = onView(
+            allOf(
+                withId(android.R.id.button1), withText("OK"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.ScrollView")),
+                        0
+                    ),
+                    3
+                )
+            )
+        )
+        appCompatButton.perform(scrollTo(), click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
